59bc844a002b5ba2f26f99324738fe8263950ae3
pess-sander <ivankosmynin5560@gmail.com>
2022-03-15 00:28:50 +0300

    Player movement fixed

--- Player.py
+++ Player.py
@@ -10,10 +10,12 @@ class Player(pygame.sprite.Sprite):
         self.x, self.y = pos
         self.w, self.h = size
 
-        self.speed = 2.0
-        self.dest_x, self.dest_y = 0, 0
+        self.speed = 0.777
         self.is_moving = False
         self.foot_x, self.foot_y = self.x + self.w//2, self.y + self.h
+
+        self.pos = pygame.Vector2(self.foot_x, self.foot_y)
+        self.destination = pygame.Vector2(0, 0)
 
         self.default_pic = pygame.transform.scale(get_res("player_default_pic"), size)
 
@@ -27,29 +29,21 @@ class Player(pygame.sprite.Sprite):
         mouse_x, mouse_y = pygame.mouse.get_pos()
         for event in events[0]:
             if event.type == pygame.MOUSEBUTTONDOWN:
-                self.dest_x, self.dest_y = mouse_x, mouse_y
+                self.destination = pygame.Vector2(mouse_x, mouse_y)
                 self.is_moving = True
 
         if self.is_moving:
-            if self.foot_x != self.dest_x or self.foot_y != self.dest_y:
-                self.move()
+            movement = self.destination - self.pos
+            if movement.length() < self.speed:
+                self.pos = self.destination
+            elif movement.length() != 0:
+                movement.normalize_ip()
+                movement *= self.speed
+                self.pos += movement
+            
+            if movement.length() != 0:
+                self.rect.midbottom = list(int(v) for v in self.pos)
             else:
                 self.is_moving = False
 
         self.draw()
-
-    def move(self):
-        x_sign, y_sign = 0, 0
-        if self.foot_x != self.dest_x:
-            x_sign = ((self.dest_x-self.foot_x)/abs(self.dest_x-self.foot_x))
-        if self.foot_y != self.dest_y:
-            y_sign = ((self.dest_y-self.foot_y)/abs(self.dest_y-self.foot_y))
-        dx, dy = 0, 0
-        if self.foot_x != self.dest_x:
-            dx = 1 * x_sign
-        if self.foot_y != self.dest_y:
-            dy = 1 * y_sign
-        self.rect.move_ip((dx, dy))
-        self.x += dx
-        self.y += dy
-        self.foot_x, self.foot_y = self.x + self.w//2, self.y + self.h
